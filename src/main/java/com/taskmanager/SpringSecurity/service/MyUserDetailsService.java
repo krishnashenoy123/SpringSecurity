5e0f2d39109d5a68b23483a21ad62da76330f82c
Krishna Shenoy <krishnashenoy123@gmail.com>
2025-03-04 19:43:58 +0530

    Connecting to Mysql and some basic functionalities

---
+++ src/main/java/com/taskmanager/SpringSecurity/service/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.taskmanager.SpringSecurity.service;
+
+import com.taskmanager.SpringSecurity.models.UserPrincipal;
+import com.taskmanager.SpringSecurity.models.Users;
+import com.taskmanager.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        System.out.println("Trying to fetch user: " + username);
+        Users user = repo.findByUsername(username);
+
+        if(user == null){
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("Exception - User Not Found for " + username);
+        }
+        System.out.println("User found: " + user.getUsername());
+        return new UserPrincipal(user);
+    }
+}
